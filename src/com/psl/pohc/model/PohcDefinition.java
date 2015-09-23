fd83fb76904470a3a730c2cfa4e42934124df2d3
yeongwei <ywlai@origineit.com>
2015-09-23 19:11:33 +0800

    Make Field names consistent with DB column names; Added enum Type to track DML type

--- src/com/psl/pohc/model/PohcDefinition.java
+++ src/com/psl/pohc/model/PohcDefinition.java
@@ -1,12 +1,11 @@
 package com.psl.pohc.model;
 
-
 public class PohcDefinition {
- 
-  public final String SUB_SYSTEM;
+
+  public final String SUBSYSTEM;
   public final String REGION;
   public final String SYSTEM;
-  public final String GROUP_NAME;
+  public final String GROUPNAME;
   public final String ID;
   public final String PHASE;
   public final String NODE_NAMES;
@@ -14,33 +13,34 @@ public class PohcDefinition {
   public final String DOMAIN;
   public final String OUTAGE_START;
   public final String OUTAGE_END;
-  
-  public PohcDefinition(
-      String subSystem, String region, String system, String groupName, String id,
-      String phase, String nodeNames, String status, String domain, String outageStart,
-      String outageEnd) {
-    
-    SUB_SYSTEM = subSystem;
+
+  private Type TYPE = Type.NONE;
+
+  public PohcDefinition(String subSystem, String region, String system,
+      String groupName, String id, String phase, String nodeNames,
+      String status, String domain, String outageStart, String outageEnd) {
+
+    SUBSYSTEM = subSystem;
     REGION = region;
     SYSTEM = system;
-    GROUP_NAME = groupName;
+    GROUPNAME = groupName;
     ID = id;
     PHASE = phase;
-    NODE_NAMES = nodeNames; 
+    NODE_NAMES = nodeNames;
     STATUS = status;
     DOMAIN = domain;
     OUTAGE_START = outageStart;
     OUTAGE_END = outageEnd;
-    
+
   }
-  
+
   public String asNameValuePair() {
     StringBuffer sb = new StringBuffer();
-    
-    sb.append("SUB_SYSTEM=\"" + SUB_SYSTEM + "\" ");
+
+    sb.append("SUB_SYSTEM=\"" + SUBSYSTEM + "\" ");
     sb.append("REGION=\"" + REGION + "\" ");
     sb.append("SYSTEM=\"" + SYSTEM + "\" ");
-    sb.append("GROUP_NAME=\"" + GROUP_NAME + "\" ");
+    sb.append("GROUP_NAME=\"" + GROUPNAME + "\" ");
     sb.append("ID=\"" + ID + "\" ");
     sb.append("PHASE=\"" + PHASE + "\" ");
     sb.append("NODE_NAMES=\"" + NODE_NAMES + "\" ");
@@ -48,7 +48,33 @@ public class PohcDefinition {
     sb.append("DOMAIN=\"" + DOMAIN + "\" ");
     sb.append("OUTAGE_START=\"" + OUTAGE_START + "\" ");
     sb.append("OUTAGE_END=\"" + OUTAGE_END + "\" ");
-    
+
     return sb.toString();
+  }
+
+  public boolean setType(Type type) {
+    TYPE = type;
+    return true;
+  }
+
+  public Type getType() {
+    return TYPE;
+  }
+
+  public enum Type {
+    INSERT, UPDATE, NONE;
+
+    public String getString() {
+      switch (this) {
+      case INSERT:
+        return "I";
+      case UPDATE:
+        return "U";
+      case NONE:
+        return "N";
+      default:
+        return "X";
+      }
+    }
   }
 }
