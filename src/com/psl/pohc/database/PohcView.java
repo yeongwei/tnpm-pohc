1ed9b5e205835b4fdc89410c605eab72c4a9cee6
yeongwei <ywlai@origineit.com>
2015-09-25 15:02:19 +0800

    Implemented insert method

--- src/com/psl/pohc/database/PohcView.java
+++ src/com/psl/pohc/database/PohcView.java
@@ -1,5 +1,6 @@
 package com.psl.pohc.database;
 
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
@@ -8,7 +9,6 @@ import com.psl.pohc.model.PohcDefinition;
 public class PohcView extends DatabaseInstance {
   
   private final static String POHC_VIEW_FQN = "POHC_VIEW";
-  private final static String DATE_FORMAT = "yyyy-mm-dd";
   private final Logger LOGGER = Logger.getLogger(PohcView.class.getName());
   
   private String DELIMITER_REGEXP = "\\r?\\n";
@@ -19,8 +19,58 @@ public class PohcView extends DatabaseInstance {
     LOGGER.info("Object has initialized successfully.");
   }
   
-  public void insert(PohcDefinition pohcDefinition) {
+  public boolean insert(PohcDefinition pohcDefinition) {
+    StringBuffer insertSql = new StringBuffer();
+    insertSql
+      .append("INSERT INTO ").append(POHC_VIEW_FQN).append(" ")
+      .append("(ID, SUBSYSTEM, REGION, SYSTEM, GROUPNAME, PHASE, NODE_NAME, STATUS, DOMAIN, OUTAGE_START, OUTAGE_END) ").append(" ")
+      .append("VALUES (")
+      .append("?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?")
+      .append(")");
+    ArrayList<PohcDefinition> pohcDefinitions = parse(pohcDefinition);
     
+    PreparedStatement preparedStatement;
+    
+    try {
+      LOGGER.info(String.format("Preparing statement for SQL - %s", insertSql));
+      preparedStatement = connection.prepareStatement(insertSql.toString());
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      return false;
+    }
+    
+    try {
+      LOGGER.info("Setting prepared statement");
+      for (PohcDefinition df : pohcDefinitions) {
+        preparedStatement.setString(1, df.ID);
+        preparedStatement.setString(2, df.SUBSYSTEM);
+        preparedStatement.setString(3, df.REGION);
+        preparedStatement.setString(4, df.SYSTEM);
+        preparedStatement.setString(5, df.GROUPNAME);
+        preparedStatement.setString(6, df.PHASE);
+        preparedStatement.setString(7, df.NODE_NAMES);
+        preparedStatement.setString(8, df.STATUS);
+        preparedStatement.setString(9, df.DOMAIN);
+        preparedStatement.setDate(10, df.OUTAGE_START);
+        preparedStatement.setDate(11, df.OUTAGE_END);
+        
+        preparedStatement.addBatch();
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      return false;
+    }
+    
+    int[] executedResult;
+    try {
+      LOGGER.info("Executing prepared statement");
+      executedResult = preparedStatement.executeBatch(); 
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      return false;
+    }
+    
+    return true;
   }
   
   public ArrayList<PohcDefinition> parse(PohcDefinition pohcDefinition) {
