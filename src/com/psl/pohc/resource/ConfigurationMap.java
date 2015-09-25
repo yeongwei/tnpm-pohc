9c76149aecb5442adf39400f76104b8f29ff99de
yeongwei <ywlai@origineit.com>
2015-09-25 19:19:00 +0800

    Added needsValidation method; Added Default enum to populate default values; Fixed lookup logics

--- src/com/psl/pohc/resource/ConfigurationMap.java
+++ src/com/psl/pohc/resource/ConfigurationMap.java
@@ -7,7 +7,8 @@ import java.util.logging.Logger;
 
 public class ConfigurationMap {
   private final static String CONFIGURATION_PROPERTY = "configuration.file";
-  private final Logger LOGGER = Logger.getLogger(ConfigurationMap.class.getName());
+  private final Logger LOGGER = Logger.getLogger(ConfigurationMap.class
+      .getName());
   private Properties CONFIGURATION;
   private boolean hasError = false;
 
@@ -23,43 +24,81 @@ public class ConfigurationMap {
 
       CONFIGURATION = new Properties();
       CONFIGURATION.load(fileInputStream);
-
+      
       for (Key k : Key.values()) {
-        LOGGER.info(String.format("Attempt to validate configuration with %s.", k.getName()));
+        if (!k.needsValidation()) {
+          LOGGER.info(String.format("Skipping validation for %s.",
+              k.getName()));
+          continue;
+        }
+        
+        LOGGER.info(String.format("Attempt to validate configuration with %s.",
+            k.getName()));
         if (CONFIGURATION.containsKey(k.getName())) {
           if (k.equals(Key.TNPM_INSTANCE)) {
-            if (!(CONFIGURATION.get(k.getName()).toString().equals("wireless") ||
-                CONFIGURATION.get(k.getName()).toString().equals("wireline"))) {
-              LOGGER.info("tnpm.instance only expect value either wireless or wireline.");
+            if (!(CONFIGURATION.get(k.getName()).toString().equals("wireless") || CONFIGURATION
+                .get(k.getName()).toString().equals("wireline"))) {
+              LOGGER
+                  .info("tnpm.instance only expect value either wireless or wireline.");
               throw new Exception(k.getMessage());
             }
           }
         } else {
-          LOGGER.info(String.format("%s is not found in configuration.", k.getName()));
+          LOGGER.info(String.format("%s is not found in configuration.",
+              k.getName()));
           throw new Exception(k.getMessage());
         }
-      } 
+      }
+      
+      for (Default d : Default.values()) {
+        LOGGER.info(
+            String.format(
+                "About to evaluate default value for %s.", 
+                Key.valueOf(d.name()).getName()));
+        
+        if (CONFIGURATION.containsKey(Key.valueOf(d.name()).getName())) {
+          LOGGER.info(
+              String.format(
+                  "Skipping default value for %s.", 
+                  Key.valueOf(d.name()).getName()));
+          continue;
+        }
+        
+        CONFIGURATION.put(
+            Key.valueOf(d.name()).getName(), 
+            d.getValue());
+      }
+      
+      for (Object x : CONFIGURATION.values()) {
+        LOGGER.info("DEBUG" + x.toString());
+      }
     } catch (Exception ex) {
       ex.printStackTrace();
       LOGGER.severe(ex.getMessage());
       hasError = true;
     }
   }
-  
+
   public boolean hasError() {
     return hasError;
   }
-  
+
   public String get(String key) {
     if (CONFIGURATION.get(key) == null) {
-      return null;
+      if (Default.valueOf(key) == null) {
+        return null;
+      } else {
+        return null;
+      }
     } else {
       return CONFIGURATION.get(key).toString();
     }
   }
 
   protected enum Key {
-    POHC_HOST, POHC_DB_PORT, POHC_DB_USER, POHC_DB_PASSWORD, POHC_DB_SCHEMA, TNPM_HOST, TNPM_DB_PORT, TNPM_DB_USER, TNPM_DB_PASSWORD, TNPM_DB_SCHEMA, TNPM_INSTANCE;
+    POHC_HOST, POHC_DB_PORT, POHC_DB_USER, POHC_DB_PASSWORD, POHC_DB_SCHEMA, 
+    TNPM_HOST, TNPM_DB_PORT, TNPM_DB_USER, TNPM_DB_PASSWORD, TNPM_DB_SCHEMA, TNPM_INSTANCE, 
+    CONFIG_DELIMITER, CONFIG_BATCH_SIZE, CONFIG_COMMIT;
 
     String getName() {
       switch (this) {
@@ -85,11 +124,17 @@ public class ConfigurationMap {
         return "tnpm.db.schema";
       case TNPM_INSTANCE:
         return "tnpm.instance";
+      case CONFIG_DELIMITER:
+        return "config.delimiter";
+      case CONFIG_BATCH_SIZE:
+        return "config.batch.size";
+      case CONFIG_COMMIT:
+        return "config.commit";
       default:
         return null;
       }
     }
-    
+
     String getMessage() {
       switch (this) {
       case POHC_HOST:
@@ -116,7 +161,51 @@ public class ConfigurationMap {
         return "Please provide the TNPM instance (E.g. wireline / wireless).";
       default:
         return null;
-      }      
+      }
+    }
+
+    boolean needsValidation() {
+      switch (this) {
+      case POHC_HOST:
+        return true;
+      case POHC_DB_PORT:
+        return true;
+      case POHC_DB_USER:
+        return true;
+      case POHC_DB_PASSWORD:
+        return true;
+      case POHC_DB_SCHEMA:
+        return true;
+      case TNPM_HOST:
+        return true;
+      case TNPM_DB_PORT:
+        return true;
+      case TNPM_DB_USER:
+        return true;
+      case TNPM_DB_PASSWORD:
+        return true;
+      case TNPM_DB_SCHEMA:
+        return true;
+      case TNPM_INSTANCE:
+        return true;
+      default:
+        return false;
+      }
+    }
+  }
+
+  protected enum Default {
+    CONFIG_BATCH_SIZE, CONFIG_COMMIT;
+
+    String getValue() {
+      switch (this) {
+      case CONFIG_BATCH_SIZE:
+        return "1000";
+      case CONFIG_COMMIT:
+        return "false";
+      default:
+        return null;
+      }
     }
   }
 }
