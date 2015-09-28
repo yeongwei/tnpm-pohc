633a2132a6a0382cfa73d2046c5c740e374463e1
yeongwei <ywlai@origineit.com>
2015-09-28 14:42:53 +0800

    Make fields / methods non static

--- src/com/psl/pohc/database/Tnpm.java
+++ src/com/psl/pohc/database/Tnpm.java
@@ -1,9 +1,18 @@
 package com.psl.pohc.database;
 
+import java.util.logging.Logger;
+
 import com.psl.pohc.model.PohcDefinition;
 
 public class Tnpm {
-  public static PohcDefinition inventoryLookup(PohcDefinition pohcDefinition) {
+  
+  private final Logger LOGGER = Logger.getLogger(Tnpm.class.getName());
+  
+  public Tnpm() {
+    LOGGER.info("Object has initialized.");
+  }
+  
+  public PohcDefinition inventoryLookup(PohcDefinition pohcDefinition) {
     pohcDefinition.setInventory(PohcDefinition.Inventory.MATCH);
     return pohcDefinition;
   }
