ac45a96579783733a6820f02d22747c90438b6ee
yeongwei <ywlai@origineit.com>
2015-09-28 17:57:53 +0800

    Wireline inventory implementation

--- src/com/psl/pohc/database/TnpmWireline.java
+++ src/com/psl/pohc/database/TnpmWireline.java
@@ -1,9 +1,34 @@
 package com.psl.pohc.database;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class TnpmWireline extends Tnpm {
 
   public TnpmWireline(String driverClass, String host, String port, String sid,
       String user, String password) throws Exception {
     super(driverClass, host, port, sid, user, password);
+  }
+  
+  @Override
+  boolean getInventory() {
+    this.INVENTORY_STORE = new ArrayList<String>();
+    String SQL = "SELECT STR_NAME FROM SE_DESC";
+    
+    Statement statement;
+    ResultSet resultSet;
+    try {
+      statement = this.connection.createStatement();
+      resultSet = statement.executeQuery(SQL);
+      
+      while (resultSet.next()) {
+        INVENTORY_STORE.add(resultSet.getString("STR_NAME"));
+      }
+    } catch (Exception ex) {
+      
+    }
+    
+    return true;
   }
 }
