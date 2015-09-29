3bf9bc30e84ed3a1126649386fe2cca08c8b7d75
yeongwei <ywlai@origineit.com>
2015-09-29 17:18:30 +0800

    To accept -configurationFile parameter

--- resource/runPohc.sh
+++ resource/runPohc.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "Please provide value for -configurationFile";
+    exit -1;
+fi
+
+CONFIG_FILE="";
+case "$1" in
+    -configurationFile)
+        if [ -f "$2" ]; then
+            CONFIG_FILE=$2;
+        else
+            echo $2 "not found.";
+            exit -1;
+        fi
+        ;;
+    *)
+        echo "-configurationFile not provided";
+        exit -1;
+        ;;
+esac;
+
+if [ -z $JAVA_HOME ]; then
+    echo "JAVA_HOME is not set!";
+    exit -1;
+fi 
+
 CURR_DIR=`pwd`
 LOG_DIR=$CURR_DIR"/log/"
 LIB_DIR=$CURR_DIR"/lib/"
@@ -8,7 +34,7 @@ CONFIG_DIR=$CURR_DIR"/configuration/"
 CONSOLE_LOG=$LOG_DIR"pohc-console.log"
 
 CONFIG_FILE_PROP="-Dconfiguration.file="
-CONFIG_FILE_PATH=$CONFIG_DIR"configuration.properties"
+CONFIG_FILE_PATH=$CONFIG_FILE
 
 ENTITY_MAP_FILE_PROP="-Dconfiguration.entity.map="
 ENTITY_MAP_FILE_PATH=$CONFIG_DIR"EntityMap.csv"
@@ -28,12 +54,7 @@ JAVA_CP=$JAVA_FULL_CLS_PATH
 
 JAVA_ARG="$JAVA_LOGGING_ARG$JAVA_LOGGING_FILE 
 $CONFIG_FILE_PROP$CONFIG_FILE_PATH 
-$ENTITY_MAP_FILE_PROP$ENTITY_MAP_FILE_PATH"
-
-if [ -z $JAVA_HOME ]; then
-    echo "JAVA_HOME is not set!";
-    exit -1;
-fi    
+$ENTITY_MAP_FILE_PROP$ENTITY_MAP_FILE_PATH"   
 
 CMD="$JAVA_HOME/bin/java 
 -cp $JAVA_CP 
