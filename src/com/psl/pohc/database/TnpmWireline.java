89db9bf35ff9a8723473461bf2ee265c7bd2d87b
yeongwei <ywlai@origineit.com>
2015-09-29 16:19:59 +0800

    Concrete implementation for wireline

--- src/com/psl/pohc/database/TnpmWireline.java
+++ src/com/psl/pohc/database/TnpmWireline.java
@@ -1,16 +1,43 @@
 package com.psl.pohc.database;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import com.psl.pohc.model.PohcDefinition;
 
 public class TnpmWireline extends Tnpm {
 
+  private PreparedStatement PREPARED_STATEMENT;
+  
   public TnpmWireline(String driverClass, String host, String port, String sid,
       String user, String password) throws Exception {
     super(driverClass, host, port, sid, user, password);
   }
+
+  @Override
+  boolean getInventory() throws Exception {
+    String sql = "SELECT STR_NAME FROM ELMT_DESC WHERE STR_NAME = ?";
+    PREPARED_STATEMENT = this.connection.prepareStatement(sql);
+    return true;
+  }
   
   @Override
   boolean checkIfExist(PohcDefinition pohcDefinition) {
-    return true;
+    boolean found = false;
+   
+    try {
+      PREPARED_STATEMENT.setString(1, pohcDefinition.NODE_NAMES);
+      ResultSet resultSet = PREPARED_STATEMENT.executeQuery();
+      if (resultSet.next()) {
+        String node = resultSet.getString(1);
+        if (node.equals(pohcDefinition.NODE_NAMES)) {
+          found = true;
+        }
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    
+    return found;
   }
 }
