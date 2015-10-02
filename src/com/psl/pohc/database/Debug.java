1cc9356e204ce25089d0b6690f676fe0468f1550
yeongwei <ywlai@origineit.com>
2015-10-02 18:08:17 +0800

    Add JDBC option

--- src/com/psl/pohc/database/Debug.java
+++ src/com/psl/pohc/database/Debug.java
@@ -2,7 +2,10 @@ package com.psl.pohc.database;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 import com.psl.pohc.Main;
@@ -18,9 +21,9 @@ public class Debug {
     if (args.length == 0) {
       throw new Exception("No option as input provided.");
     }
-    
+
     Debug d = new Debug();
-    d.run(args[0]);
+    d.run(args);
   }
 
   public Debug() throws Exception {
@@ -32,25 +35,73 @@ public class Debug {
         CONFIGURATION_MAP.get("pohc.db.user"),
         CONFIGURATION_MAP.get("pohc.db.password"));
     POHC.setTableName(CONFIGURATION_MAP.get("pohc.db.table"));
-    
+
     LOGGER.info("Object initialized successfully.");
   }
 
-  public void run(String option) throws Exception {
-    LOGGER.info(String.format("About to run %s.", option));
-    switch (Option.valueOf(option)) {
+  public void run(String... option) throws Exception {
+    LOGGER.info(String.format("About to run %s.", option[0]));
+    Connection connection;
+    DatabaseMetaData databaseMetadata;
+    Statement statement;
+    ResultSet resultSet;
+    boolean status;
+    switch (Option.valueOf(option[0])) {
     case POHC_SHOW_ALL_TABLE_AND_VIEWS_FROM_SMDBUSER3:
-      Connection connection = POHC.connection;
-      DatabaseMetaData databaseMetadata = connection.getMetaData();
-      ResultSet resultSet = databaseMetadata.getTables(
-            null, "SMDBUSER3", null, null);
-      while(resultSet.next()) {
+      connection = POHC.connection;
+      databaseMetadata = connection.getMetaData();
+      resultSet = databaseMetadata.getTables(null, "SMDBUSER3", null,
+          null);
+      while (resultSet.next()) {
         StringBuffer result = new StringBuffer();
-        result
-          .append("TABLE_SCHEM|_|").append(resultSet.getString("TABLE_SCHEM"))
-          .append("|_|")
-          .append("TABLE_NAME|_|").append(resultSet.getString("TABLE_NAME"));
+        result.append("TABLE_SCHEM|_|")
+            .append(resultSet.getString("TABLE_SCHEM")).append("|_|")
+            .append("TABLE_NAME|_|").append(resultSet.getString("TABLE_NAME"));
         LOGGER.info(result.toString());
+      }
+      break;
+    case POHC_SHOW_ALL_TABLE_AND_VIEWS_FROM_SMDBUSER:
+      connection = POHC.connection;
+      databaseMetadata = connection.getMetaData();
+      resultSet = databaseMetadata.getTables(null, "SMDBUSER", null,
+          null);
+      while (resultSet.next()) {
+        StringBuffer result = new StringBuffer();
+        result.append("TABLE_SCHEM|_|")
+            .append(resultSet.getString("TABLE_SCHEM")).append("|_|")
+            .append("TABLE_NAME|_|").append(resultSet.getString("TABLE_NAME"));
+        LOGGER.info(result.toString());
+      }
+      break;
+    case POHC_SELECT_FROM_TABLE_BASED_ON_NAME:
+      connection = POHC.connection;
+      statement = connection.createStatement();
+      LOGGER.info("About to execute Test SQL.");
+      resultSet = statement.executeQuery("SELECT ROWNUM RNUM FROM " + option[1]);
+      LOGGER.info("About iterate results.");
+      int count = 1;
+      while (resultSet.next()) {
+        StringBuffer result = new StringBuffer();
+        result.append(count).append("|_|").append(resultSet.getObject(1));
+        LOGGER.info(result.toString());
+        count += 1;
+      }
+      break;
+    case JDBC: // supports oracle only
+      LOGGER.info("Only Oracle supported for now.");
+      // CMD - JDBC HOST_NAME PORT SID USERNAME PASSWORD SQL
+      Properties info = new Properties();
+      info.put("user", option[4]);
+      info.put("password", option[5]);
+      connection = DriverManager.getConnection(
+          Jdbc.getUrl("oracle.jdbc.driver.OracleDriver", option[1], option[2], option[3]), 
+          info);
+      statement = connection.createStatement();
+      status = statement.execute(option[6]);
+      if (status) {
+        LOGGER.info("Execution has Result Set.");
+      } else {
+        LOGGER.info("Execution has NO Result Set.");
       }
       break;
     default:
@@ -60,6 +111,7 @@ public class Debug {
   }
 
   enum Option {
-    POHC_SHOW_ALL_TABLE_AND_VIEWS_FROM_SMDBUSER3
+    POHC_SHOW_ALL_TABLE_AND_VIEWS_FROM_SMDBUSER3, POHC_SHOW_ALL_TABLE_AND_VIEWS_FROM_SMDBUSER,
+    POHC_SELECT_FROM_TABLE_BASED_ON_NAME, JDBC;
   }
 }
